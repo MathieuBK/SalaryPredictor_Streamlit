11b7225d7ca5e8a3d1987ae614b17436fdd71f34
MathieuBK <45979854+MathieuBK@users.noreply.github.com>
2025-06-25 12:41:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,17 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
+# This MUST be the first Streamlit command before any other UI elements
+st.set_page_config(page_title="DevPayMaster", page_icon="💼", layout="centered")
 
 def main():
-	st.set_page_config(page_title="DevPayMaster", page_icon="💼", layout="centered")
-
-	st.sidebar.title("Navigation")
-	page = st.sidebar.radio("Go to", ["Predict", "Explore"])
+	page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
 	if page == "Predict":
 		show_predict_page()
 	else:
 		show_explore_page()
 
-
 if __name__ == "__main__":
 	main()
+
