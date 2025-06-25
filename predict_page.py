c702b2d777f457e826814a7499b3e1a788fb9e61
MathieuBK <45979854+MathieuBK@users.noreply.github.com>
2025-06-25 12:28:10 +0200

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -1,66 +1,84 @@
+"""
+predict_page.py
+
+Provides a Streamlit interface for predicting developer salaries based on
+country, education level, and years of experience using a pre-trained model.
+"""
+
 import streamlit as st
 import pickle
 import numpy as np
 
 
-def load_model():
-    with open('saved_steps.pkl', 'rb') as file:
-        data = pickle.load(file)
-    return data
+@st.cache_resource
+def load_model(path="saved_steps.pkl"):
+	"""
+	Load the trained regression model and label encoders from a pickle file.
 
-data = load_model()
+	Args:
+		path (str): Path to the pickle file.
 
-regressor = data["model"]
-le_country = data["le_country"]
-le_education = data["le_education"]
+	Returns:
+		dict: Contains 'model', 'le_country', 'le_education'.
+	"""
+	with open(path, "rb") as file:
+		data = pickle.load(file)
+	return data
+
 
 def show_predict_page():
-    st.title("💵 Developer Salary Predictor")
-    # st.caption("Based on Stack Overflow Developer Survey 2023 -> https://insights.stackoverflow.com/survey")
-    st.caption("Based on 90,000+ responses to Stack Overflow's Developer Survey 2023 (https://insights.stackoverflow.com/survey).")
+	"""
+	Render the salary prediction page with input widgets and prediction output.
+	"""
+	st.title("💵 Developer Salary Predictor")
+	st.caption(
+		"Based on 90,000+ responses to Stack Overflow's Developer Survey 2023 "
+		"(https://insights.stackoverflow.com/survey)."
+	)
 
-    st.title("")
+	countries = (
+		"United States",
+		"India",
+		"United Kingdom",
+		"Germany",
+		"Canada",
+		"Brazil",
+		"France",
+		"Spain",
+		"Australia",
+		"Netherlands",
+		"Poland",
+		"Italy",
+		"Russian Federation",
+		"Sweden",
+	)
 
-    # st.write("""### Enter information to predict the salary""")
-    # st.title("")
+	education_levels = (
+		"Less than a Bachelors",
+		"Bachelor’s degree",
+		"Master’s degree",
+		"Post grad",
+	)
 
-    countries = (
-        "United States",
-        "India",
-        "United Kingdom",
-        "Germany",
-        "Canada",
-        "Brazil",
-        "France",
-        "Spain",
-        "Australia",
-        "Netherlands",
-        "Poland",
-        "Italy",
-        "Russian Federation",
-        "Sweden",
-    )
+	country = st.selectbox("Country", countries)
+	education = st.selectbox("Education Level", education_levels)
+	experience = st.slider("Years of Experience", 0, 50, 3)
 
-    education = (
-        "Less than a Bachelors",
-        "Bachelor’s degree",
-        "Master’s degree",
-        "Post grad",
-    )
+	if st.button("Calculate Salary", type="primary"):
+		data = load_model()
 
-    country = st.selectbox("Country", countries)
-    education = st.selectbox("Education Level", education)
+		regressor = data["model"]
+		le_country = data["le_country"]
+		le_education = data["le_education"]
 
-    expericence = st.slider("Years of Experience", 0, 50, 3)
-    st.write("")
-    st.write("")
+		# Prepare feature vector for prediction
+		X = np.array([[country, education, experience]])
+		X[:, 0] = le_country.transform(X[:, 0])
+		X[:, 1] = le_education.transform(X[:, 1])
+		X = X.astype(float)
 
-    ok = st.button("Calculate Salary", type="primary")
-    if ok:
-        X = np.array([[country, education, expericence ]])
-        X[:, 0] = le_country.transform(X[:,0])
-        X[:, 1] = le_education.transform(X[:,1])
-        X = X.astype(float)
+		predicted_salary = regressor.predict(X)[0]
+		# Scaling salary as per original code (1.5 factor)
+		scaled_salary = 1.5 * predicted_salary
 
-        salary = regressor.predict(X)
-        st.subheader(f"The estimated salary is :green[${1.5*salary[0]:.0f}]")
+		st.subheader(f"The estimated salary is :green[${scaled_salary:.0f}]")
